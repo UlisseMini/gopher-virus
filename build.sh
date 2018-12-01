56679f3a6730bf9fd67b4c7b0eced960c6ebc390
ScriptKidy <ulisse.mini@gmail.com>
2018-12-01 13:21:28 -0500

    this is so sad, alexa play despasito

--- build.sh
+++ build.sh
@@ -3,18 +3,19 @@
 # Just in case the global aliases are not loaded
 alias wgo="env GOOS=windows go build -ldflags -H=windowsgui"
 
-# format and build payload
+# format everything first
+gofmt -w -l -s .
+
+# build payload
 printf "go build payload.go\n"
 cd ./payload
-go fmt payload.go 2>/dev/null
 wgo payload.go
 mv payload.exe .. 2>/dev/null
 cd ..
 
-# format and build deploy
+# build deploy
 printf "go build deploy.go\n"
 cd ./deploy
-go fmt deploy.go 2>/dev/null
 wgo deploy.go
 mv deploy.exe .. 2>/dev/null
 cd ..
